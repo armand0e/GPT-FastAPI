8f72584886708850b66cf2693ebecd1e83e099ba
Arman Rafiee <aran.rafiee@gmail.com>
2025-02-14 19:56:25 -0500

    latest

---
+++ start_server.bat
@@ -0,0 +1,7 @@
+@echo off
+REM  • new-tab: Runs cmd.exe with our cloudflared loop.
+REM  • ; split-pane: Splits horizontally (-V) running cmd.exe with our Python loop.
+REM Note: Commands are separated by a semicolon.
+copy config.yml C:\Users\aranr\.cloudflared\config.yml
+wt new-tab -p "Command Prompt" cmd.exe /k "cloudflared tunnel run" ; split-pane -V -p "Command Prompt" cmd.exe /k "cd \Users\aranr\Documents\GitHub\GPT-FastAPI && python src\main.py"
+
